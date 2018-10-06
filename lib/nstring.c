9d4a70a6e6f7b604ec586012df9008d0bc8e34b1
noyesno <noyesno.net@gmail.com>
2018-10-06 09:26:55 +0800

    enhance nstring_match() for performance and add test for it

--- lib/nstring.c
+++ lib/nstring.c
@@ -6,12 +6,21 @@
  | This file is covered by the BSD License.                   |
  *------------------------------------------------------------*/
 
-#include "nstring.h"
+#ifndef TEST_MAIN
+  #include "nstring.h"
+#else
+  #include <stdio.h>
+  #include <string.h>
+#endif
 
 int nstring_match(
   const char *pattern, int pattern_size,
   const char *string, int string_size)
 {
+  #ifdef TEST_MAIN
+  printf("\tdebug: %.*s | %.*s\n", pattern_size, pattern, string_size, string);
+  #endif
+
   const char *p = pattern;
   const char *s = string;
   int s_size = string_size;
@@ -22,6 +31,7 @@ int nstring_match(
       case '?' :
         if( s_size>0 ){
           s++; s_size--;
+          p++; p_size--;
         }else{
           return 0;
         }
@@ -41,12 +51,72 @@ int nstring_match(
       default:
         if( s_size>0 && p[0]==s[0] ){
           s++; s_size--;
+          p++; p_size--;
         } else {
           return 0;
         }
     }
-    p++; p_size--;
+
+    if(p_size==0) break;
+
+    switch(p[p_size-1]){
+      case '?' :
+        if( s_size>0 ){
+          s_size--;
+          p_size--;
+        }else{
+          return 0;
+        }
+        break;
+      case '*' :
+        break;
+      default :
+        if( s_size>0 && p[p_size-1]==s[s_size-1] ){
+          s_size--;
+          p_size--;
+        } else {
+          return 0;
+        }
+    }
   }
 
   return s_size==0?1:0;
 }
+
+
+#ifdef TEST_MAIN
+
+int main(int argc, char *argv[]){
+  const char *pattern;
+  const char *string;
+  if( argc==3 ){
+    pattern = argv[1];
+    string  = argv[2];
+    int match = nstring_match(pattern, strlen(pattern), string, strlen(string));
+    printf("%d %s %s\n", match, pattern, string);
+    return 0;
+  }
+
+  const char *test_case[] = {
+    "abc*123",   "abcd1234",  "0",
+    "abc*123?",  "abcd1234",  "1",
+    "abc*123?",  "abcd12345", "0",
+    "abc*123*",  "abcd12345", "1",
+    NULL, NULL
+  };
+
+  for(int i=0; 1 ; i += 3 ){
+    const char *pattern = test_case[i];
+    const char *string  = test_case[i+1];
+    const char *result  = test_case[i+2];
+    if( !(pattern && string) ) break;
+    printf("+++ #%d nstring_match %s %s\n", (i/3+1), pattern, string);
+    int match = nstring_match(pattern, strlen(pattern), string, strlen(string));
+    printf("=== %s %d # nstring_match \"%s\" \"%s\"\n",
+      (result[0]-0x30)==match?"PASS":"FAIL",
+      match, pattern, string );
+  }
+  return 0;
+}
+
+#endif
