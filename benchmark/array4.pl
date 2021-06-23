50f7e7a58c36dcdb9e16b4b08f62287d30cf9d4c
James Parkinson <james.parkinson@bcf.org.au>
2021-06-23 17:33:44 +1000

    Benchmarking Makefile to run benchmarks Add array4 tests for integer indexes

---
+++ benchmark/array4.pl
@@ -0,0 +1,10 @@
+#!/usr/local/bin/perl
+
+$base = 100000;
+
+for ($i=0; $i<$base; $i++) {
+	$arr1{$i} = $base - $i;
+}
+for ($i=5; $i<$base; $i++) {
+	$arr1{$i-1} = $arr1{$i};
+}
---
+++ benchmark/array4.tcl
@@ -0,0 +1,9 @@
+set base 100000;
+
+for {set i 0} {$i<$base} {incr i} {
+  set arr1($i) [expr $base - $i]
+}
+
+for {set i 5} {$i<$base} {incr i} {
+  set arr1([expr $i - 1]) $arr1($i)
+}
